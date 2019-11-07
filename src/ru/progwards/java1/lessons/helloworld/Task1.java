61cc4c38effd5e3ebfd51dc09352267d44db9213
Arseniy Lesechko <a.lesechko@ibolit.pro>
2019-11-07 11:17:27 +0300

    Task1

---
+++ src/ru/progwards/java1/lessons/helloworld/Task1.java
@@ -0,0 +1,17 @@
+package ru.progwards.java1.lessons.helloworld;
+
+public class Task1 {
+    public static void main(String[] args) {
+        String msg1 = "Хорошо идут дела";
+        String msg2 = "Изучаю Java я!";
+        String msg3 = " ";
+        System.out.println(msg1);
+        System.out.println(msg2);
+        System.out.print(msg1);
+        System.out.print(msg3);
+        System.out.println(msg2);
+        System.out.print(msg2);
+        System.out.print(msg3);
+        System.out.println(msg1);
+    }
+}

12643205872491a67f345dbafd0c87e7b7cb118c
Arseniy Lesechko <a.lesechko@ibolit.pro>
2019-11-07 12:32:58 +0300

    add Task2

--- src/ru/progwards/java1/lessons/helloworld/Task1.java
+++ src/ru/progwards/java1/lessons/helloworld/Task1.java
@@ -5,6 +5,7 @@ public class Task1 {
         String msg1 = "Хорошо идут дела";
         String msg2 = "Изучаю Java я!";
         String msg3 = " ";
+
         System.out.println(msg1);
         System.out.println(msg2);
         System.out.print(msg1);

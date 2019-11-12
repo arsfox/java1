3b0f45ae7882081a91d0eb7104d54f3b76e0f7ee
Arseniy Lesechko <a.lesechko@ibolit.pro>
2019-11-12 17:03:27 +0300

    add HM 2

--- src/ru/progwards/java1/lessons/basics/ReverseDigits.java
+++ src/ru/progwards/java1/lessons/basics/ReverseDigits.java
@@ -2,7 +2,11 @@ package ru.progwards.java1.lessons.basics;
 
 public class ReverseDigits {
 
+    public static void main(String[] args){
+        System.out.println(reverseDigits(876));
+    }
+
     public static int reverseDigits(int number) {
-        return 0;
+        return (number % 10 * 100) + (number / 10 % 10 * 10) + (number / 100 % 10);
     }
 }

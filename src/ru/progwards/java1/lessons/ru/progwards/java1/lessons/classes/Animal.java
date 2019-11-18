dd9113fd12e991342bec7df5317657c5d4a3ee00
Arseniy Lesechko <arseniy.le@yandex.ru>
2019-11-18 23:59:08 +0300

    add new home work

---
+++ src/ru/progwards/java1/lessons/ru/progwards/java1/lessons/classes/Animal.java
@@ -0,0 +1,12 @@
+package ru.progwards.java1.lessons.ru.progwards.java1.lessons.classes;
+
+/**
+ * Created by Arseniy on 18.11.2019.
+ */
+public class Animal {
+
+    public AnimalKind getKind(){
+        return AnimalKind.ANIMAL;
+    }
+
+}
---
+++ src/ru/progwards/java1/lessons/ru/progwards/java1/lessons/classes/AnimalKind.java
@@ -0,0 +1,8 @@
+package ru.progwards.java1.lessons.ru.progwards.java1.lessons.classes;
+
+/**
+ * Created by Arseniy on 18.11.2019.
+ */
+public enum AnimalKind {
+    ANIMAL, COW, HAMSTER, DUCK
+}

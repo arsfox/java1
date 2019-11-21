431bb16491eb216ef5b5311d283447fb044967c2
Arseniy Lesechko <a.lesechko@ibolit.pro>
2019-11-21 16:11:46 +0300

    all for victory!

--- src/ru/progwards/java1/lessons/bitsworld/CheckBit.java
+++ src/ru/progwards/java1/lessons/bitsworld/CheckBit.java
@@ -1,24 +1,19 @@
 package ru.progwards.java1.lessons.bitsworld;
 
+import com.sun.tools.javac.util.Assert;
+
+import java.util.concurrent.ThreadLocalRandom;
+
 /**
  * Created by Arseniy on 20.11.2019.
  */
 public class CheckBit {
 
     public static void main(String[] args) {
-        System.out.println(checkBit((byte)-114, 1));
-        int a = 142;
-        byte b = (byte) a;
-        System.out.println(b);
-        System.out.println(Integer.toBinaryString(a));
+
     }
 
     public static int checkBit(byte value, int bitNumber) {
-        int result = 0;
-        int n = value;
-        for (int i = 1; i < bitNumber; i++) {
-            result = (n >> i) & 1;
-        }
-        return result;
+        return (value >> bitNumber) & 0b00000001;
     }
 }

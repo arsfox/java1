b4e4ae14085286b090d4c5782edd84fb8eebf49c
Arseniy Lesechko <arseniy.le@yandex.ru>
2019-11-25 22:02:05 +0300

    add ll task

---
+++ src/ru/progwards/java1/lessons/interfaces/CalculateFibonacci.java
@@ -0,0 +1,45 @@
+package ru.progwards.java1.lessons.interfaces;
+
+/**
+ * Created by Arseniy on 25.11.2019.
+ */
+public class CalculateFibonacci {
+
+    private static CacheInfo lastFibo;
+
+    public CalculateFibonacci() {
+        lastFibo = new CacheInfo();
+    }
+
+    public static int fiboNumber(int n) {
+
+        if (lastFibo.n == n){
+            return lastFibo.fibo;
+        }
+
+        int result;
+
+        int n0 = 1;
+        int n1 = 1;
+        int n2 = 0;
+        if (n < 0){
+            result = 0;
+        } else if(n == 1 || n == 2) {
+            result = 1;
+        } else {
+            for(int i = 3; i <= n; i++){
+                n2=n0+n1;
+                n0=n1;
+                n1=n2;
+            }
+            result = n2;
+        }
+        lastFibo.fibo = result;
+        return result;
+    }
+
+    static class CacheInfo {
+        int n;
+        int fibo;
+    }
+}

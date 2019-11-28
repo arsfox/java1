223ed729e07923dbdb3045261d5bcb3b6fdeaf7b
Arseniy Lesechko <arseniy.le@yandex.ru>
2019-11-28 22:48:31 +0300

    add task

--- src/ru/progwards/java1/lessons/bigints/ByteInteger.java
+++ src/ru/progwards/java1/lessons/bigints/ByteInteger.java
@@ -3,8 +3,10 @@ package ru.progwards.java1.lessons.bigints;
 /**
  * Created by Arseniy on 28.11.2019.
  */
-public class AbsInteger {
+public class ByteInteger extends AbsInteger {
 
+    public ByteInteger(byte byteInteger) {
 
+    }
 
 }

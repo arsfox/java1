c5f98afff6a3aa5ef12bc5d56b425e49adaadc43
Arseniy Lesechko <a.lesechko@ibolit.pro>
2019-11-29 18:13:57 +0300

    add new hw

--- src/ru/progwards/java1/lessons/bigints/ArrayInteger.java
+++ src/ru/progwards/java1/lessons/bigints/ArrayInteger.java
@@ -2,6 +2,7 @@ package ru.progwards.java1.lessons.bigints;
 
 import java.math.BigDecimal;
 import java.math.BigInteger;
+import java.util.Arrays;
 
 public class ArrayInteger {
 
@@ -20,19 +21,36 @@ public class ArrayInteger {
 
     public void fromInt(BigDecimal value) {
         BigInteger valueInteger = value.toBigInteger();
-        BigInteger valueIntegerS = value.toBigInteger();
-        while (!valueInteger.equals(BigInteger.ZERO)) {
-            valueInteger = valueInteger.divide(BigInteger.TEN);
-
-            System.out.println(valueInteger.remainder(BigInteger.valueOf(10)));
+//        while (!valueInteger.equals(BigInteger.ZERO)) {
+//            valueInteger = valueInteger.divide(BigInteger.TEN);
+//
+//            System.out.println(valueInteger.remainder(BigInteger.valueOf(10)));
+//        }
+        int count = 0;
+        for (; !valueInteger.equals(BigInteger.ZERO); valueInteger = valueInteger.divide(BigInteger.TEN)){
+            digits[count] = valueInteger.remainder(BigInteger.valueOf(10)).byteValueExact();
+//            System.out.println(valueInteger.remainder(BigInteger.valueOf(10)));
+            count++;
         }
 
-
+        System.out.println(Arrays.toString(digits));
 //        while (value.compareTo(BigDecimal.valueOf(0)) == 1) {
 //            value = value.divide(BigDecimal.valueOf(10));
 //            System.out.println(value);
 //            System.out.println(1);
 //        }
+    }
+
+    public BigDecimal toInt() {
+        BigInteger result = new BigInteger("0");
+        for (int count = digits.length; count > 0; count--){
+            BigInteger r = new BigInteger("10").pow(count);
+            BigInteger digitsIter = new BigInteger(String.valueOf(digits[count - 1]));
+            BigInteger t = r.multiply(digitsIter);
+            result = result.add(t);
+        }
+        System.out.println(result.divide(new BigInteger("10")));
+        return new BigDecimal("1");
     }
 
 //    private int getCountsOfDigits(long number) {

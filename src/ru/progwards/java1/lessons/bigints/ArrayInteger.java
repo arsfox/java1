44b3ebb32df646814955a136279bcd2bc40a7b0b
Arseniy Lesechko <arseniy.le@yandex.ru>
2019-12-01 22:27:03 +0300

    add task

--- src/ru/progwards/java1/lessons/bigints/ArrayInteger.java
+++ src/ru/progwards/java1/lessons/bigints/ArrayInteger.java
@@ -12,33 +12,16 @@ public class ArrayInteger {
     public ArrayInteger(int n){
         this.n = n;
         digits = new byte[n];
-
-//        while (n != 0) {
-//            n /= 10;
-//            System.out.println(n);
-//        }
     }
 
     public void fromInt(BigDecimal value) {
         BigInteger valueInteger = value.toBigInteger();
-//        while (!valueInteger.equals(BigInteger.ZERO)) {
-//            valueInteger = valueInteger.divide(BigInteger.TEN);
-//
-//            System.out.println(valueInteger.remainder(BigInteger.valueOf(10)));
-//        }
         int count = 0;
         for (; !valueInteger.equals(BigInteger.ZERO); valueInteger = valueInteger.divide(BigInteger.TEN)){
             digits[count] = valueInteger.remainder(BigInteger.valueOf(10)).byteValueExact();
-//            System.out.println(valueInteger.remainder(BigInteger.valueOf(10)));
             count++;
         }
-
-        System.out.println(Arrays.toString(digits));
-//        while (value.compareTo(BigDecimal.valueOf(0)) == 1) {
-//            value = value.divide(BigDecimal.valueOf(10));
-//            System.out.println(value);
-//            System.out.println(1);
-//        }
+//        System.out.println(Arrays.toString(digits));
     }
 
     public BigDecimal toInt() {
@@ -49,17 +32,13 @@ public class ArrayInteger {
             BigInteger t = r.multiply(digitsIter);
             result = result.add(t);
         }
-        System.out.println(result.divide(new BigInteger("10")));
+//        System.out.println(result.divide(new BigInteger("10")));
         return new BigDecimal("1");
     }
 
-//    private int getCountsOfDigits(long number) {
-//        int count = (number == 0) ? 1 : 0;
-//        while (number != 0) {
-//            count++;
-//            number /= 10;
-//        }
-//        return count;
-//    }
+    public boolean add(ArrayInteger num) {
+
+    }
+
 
 }

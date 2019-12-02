f98aacf9ceae0ef4dbf02a746dd3f9e288a841fe
Arseniy Lesechko <arseniy.le@yandex.ru>
2019-12-02 23:05:34 +0300

    add task

--- src/ru/progwards/java1/lessons/bigints/AbsInteger.java
+++ src/ru/progwards/java1/lessons/bigints/AbsInteger.java
@@ -7,10 +7,14 @@ import java.math.BigInteger;
 public class AbsInteger {
 
     static public AbsInteger add(AbsInteger num1, AbsInteger num2) {
-        BigInteger bigNum1 = new BigInteger(num1.toString());
-        BigInteger bigNum2 = new BigInteger(num2.toString());
-
-        BigInteger bigResult = bigNum1.add(bigNum2);
+        String operandOne = "";
+        String operandTwo = "";
+        if(num1.getClass().equals(ByteInteger.class)) {
+            operandOne = num1.;
+        } else {
+            operandOne = num1.toString();
+            operandTwo = num2.toString();
+        }
 
         int result = bigResult.intValue();
 

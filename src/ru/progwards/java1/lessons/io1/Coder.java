73b6197c49fff4607d3a60a3ddd3e60a9da149c9
Arseniy Lesechko <a.lesechko@ibolit.pro>
2019-12-05 16:20:07 +0300

    fix

--- src/ru/progwards/java1/lessons/io1/Coder.java
+++ src/ru/progwards/java1/lessons/io1/Coder.java
@@ -10,20 +10,39 @@ public class Coder {
     public static void codeFile(String inFileName, String outFileName, char[] code, String logName) {
 
         try {
+
+            String outFileString = "";
+
             BufferedReader bufferedReader = new BufferedReader(new FileReader(inFileName));
             int symbol = bufferedReader.read();
             while (symbol != -1) {
-                char c = (char) symbol;
-                for(char cod : code){
-                    if
-                }
-                symbol = bufferedReader.read(); // Читаем символ
+                char codeChar = code[symbol];
+                outFileString += codeChar;
+                symbol = bufferedReader.read();
             }
+
+            setText(outFileName);
         } catch (FileNotFoundException e) {
+            try {
+                setText(e.getMessage());
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
             e.printStackTrace();
         } catch (IOException e) {
+            try {
+                setText(e.getMessage());
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
             e.printStackTrace();
         }
+    }
+
+    private static void setText(String fileName) throws IOException {
+        FileWriter writer = new FileWriter(fileName, false);
+        writer.write(fileName);
+        writer.close();
     }
 
 }

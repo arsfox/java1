6ef8e918b8dd09ee6d1d590c6487c4cbce91615c
Arseniy Lesechko <a.lesechko@ibolit.pro>
2019-12-10 11:20:49 +0300

    add

--- src/ru/progwards/java1/lessons/io2/PhoneNumber.java
+++ src/ru/progwards/java1/lessons/io2/PhoneNumber.java
@@ -7,27 +7,47 @@ public class PhoneNumber {
 
     public static String format(String phone) {
         String number = "";
-        int c = 0;
         char[] numArray = phone.toCharArray();
-
         for (int i = 0; i < numArray.length; i++) {
             if (Character.isDigit(numArray[i])) {
                 number += numArray[i];
             }
         }
 
-        if((number.length() >= 11) || (number.length() < 10)){
+        if((number.length() < 10)||(number.length() > 11)){
             try {
                 throw new Exception("Wrong number length");
             } catch (Exception e) {
                 e.printStackTrace();
             }
         }
-        System.out.println(number);
 
+        if((number.substring(0, 1).equals("8"))&&(number.length() == 11)) {
+            number = number.substring(1);
+        } else
+        if((number.substring(0, 1).equals("7"))&&(number.length() == 11)) {
+            number = number.substring(1);
+        } else
+        if((number.substring(0, 2).equals("+7"))&&(number.length() == 12)) {
+            number = number.substring(2);
+        }
 
-
-        return "";
+        number = "+7"+number;
+        char[] numberArray = number.toCharArray();
+        number = "";
+        for (int count = 0; count < numberArray.length; count++) {
+            if(count == 2){
+                number += "(";
+            }
+            if(count == 5){
+                number += ")";
+            }
+            if(count == 8){
+                number += "-";
+            }
+            number += numberArray[count];
+        }
+        return number;
     }
 
 }

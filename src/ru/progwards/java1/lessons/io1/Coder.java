6c6d23e1f37e661c836f35d7c4fac632eb8a6097
Arseniy Lesechko <a.lesechko@ibolit.pro>
2019-12-11 13:20:45 +0300

    fix

--- src/ru/progwards/java1/lessons/io1/Coder.java
+++ src/ru/progwards/java1/lessons/io1/Coder.java
@@ -8,34 +8,24 @@ import java.io.*;
 public class Coder {
 
     public static void codeFile(String inFileName, String outFileName, char[] code, String logName) {
-
         try {
-
             String outFileString = "";
-
             BufferedReader bufferedReader = new BufferedReader(new FileReader(inFileName));
             int symbol = bufferedReader.read();
             while (symbol != -1) {
-                try {
                 outFileString += code[symbol];
-                } catch (ArrayIndexOutOfBoundsException e) {
-                    setText(logName, e.getMessage());
-                }
                 symbol = bufferedReader.read();
             }
             bufferedReader.close();
             setText(outFileName, outFileString);
-        } catch (FileNotFoundException e) {
-                setText(logName, e.getMessage());
         } catch (IOException e) {
-                setText(logName, e.getMessage());
+            setText(logName, e.getMessage());
         }
     }
 
     private static void setText(String fileName, String text)  {
-        FileWriter writer = null;
         try {
-            writer = new FileWriter(fileName, false);
+            FileWriter writer = new FileWriter(fileName, false);
             writer.write(text);
             writer.close();
         } catch (IOException e) {

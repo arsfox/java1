68de9613911787d0fd2aa98a6c0aed63001ac531
Arseniy Lesechko <a.lesechko@ibolit.pro>
2019-12-11 12:18:30 +0300

    fix

--- src/ru/progwards/java1/lessons/io2/PhoneNumber.java
+++ src/ru/progwards/java1/lessons/io2/PhoneNumber.java
@@ -23,7 +23,7 @@ public class PhoneNumber {
             }
         }
 
-        if((number.substring(0, 1).equals("8"))&&(number.length() == 11)) {
+        if((number.substring(0, 1).equals("8"))&&(number.length() > 9)) {
             number = number.substring(1);
         } else
         if((number.length() == 12)&&(number.substring(0, 1).equals("7"))) {

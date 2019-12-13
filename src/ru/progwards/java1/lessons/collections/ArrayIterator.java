ebbba166ce70fd137488bb90e9e2cfc7052b1287
Arseniy Lesechko <a.lesechko@ibolit.pro>
2019-12-13 16:34:32 +0300

    fix

---
+++ src/ru/progwards/java1/lessons/collections/ArrayIterator.java
@@ -0,0 +1,29 @@
+package ru.progwards.java1.lessons.collections;
+
+import java.util.Iterator;
+
+public class ArrayIterator<T> implements Iterator<T> {
+
+    private T[] array;
+    private int length;
+    private int currentPosition;
+
+    ArrayIterator(T[] array) {
+        this.array = array;
+        this.currentPosition = 0;
+    }
+
+    @Override
+    public boolean hasNext() {
+        return this.currentPosition <= this.array.length -1 ;
+    }
+
+    @Override
+    public T next() {
+        T result = this.array[this.currentPosition];
+        this.currentPosition += 1;
+        return result;
+    }
+
+
+}

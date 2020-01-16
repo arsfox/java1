0982525bc31da94ae384138b7490ed241dab1446
Arseniy Lesechko <a.lesechko@ibolit.pro>
2020-01-16 16:37:31 +0300

    add

--- src/ru/progwards/java1/lessons/sets/ProductAnalytics.java
+++ src/ru/progwards/java1/lessons/sets/ProductAnalytics.java
@@ -18,8 +18,8 @@ public class ProductAnalytics {
     public Set<Product> existInAll() { // товары из products, которые имеются во всех магазинах
         List<Product> intersection = this.products;
 
-        for (int i = 0; i <= this.shops.size(); i++) {
-            for (int j = 0; j <= this.products.size(); j++) {
+        for (int i = 0; i < this.shops.size(); i++) {
+            for (int j = 0; j < this.products.size(); j++) {
                 if(!this.shops.get(i).getProducts().contains(this.products.get(j))){
                     intersection.remove(this.products.get(j));
                 }

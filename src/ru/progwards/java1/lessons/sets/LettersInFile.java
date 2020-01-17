6f086979951edd0f0c2802d06f2cf0d7cebde88b
Arseniy Lesechko <a.lesechko@ibolit.pro>
2020-01-17 12:17:49 +0300

    add

--- src/ru/progwards/java1/lessons/sets/LettersInFile.java
+++ src/ru/progwards/java1/lessons/sets/LettersInFile.java
@@ -1,9 +1,38 @@
 package ru.progwards.java1.lessons.sets;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.TreeSet;
+
 public class LettersInFile {
 
-    public static String process(String fileName) {
-        return "";
+    public static String process(String fileName) throws IOException {
+        Set<Character> character = new TreeSet<>();
+
+        BufferedReader bufferedReader = new BufferedReader(new FileReader(fileName));
+        int symbol = bufferedReader.read();
+        while (symbol != -1) {
+            char c = (char) symbol;
+            System.out.println(c);
+            if(Character.isAlphabetic(c)) {
+                character.add(c);
+            }
+        }
+        bufferedReader.close();
+
+        return toString(character);
+    }
+
+    private static String toString(Set<Character> character) {
+        String result = "";
+        for (Character c : character) {
+            result += c;
+        }
+
+        return result;
     }
 
 }

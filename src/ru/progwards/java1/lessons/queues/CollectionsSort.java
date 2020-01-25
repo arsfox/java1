218611f0201dbd213a5eb30b63611ab266f101fb
Arseniy Lesechko <arseniy.le@yandex.ru>
2020-01-25 14:05:22 +0300

    1

--- src/ru/progwards/java1/lessons/queues/CollectionsSort.java
+++ src/ru/progwards/java1/lessons/queues/CollectionsSort.java
@@ -5,10 +5,27 @@ import java.util.*;
 public class CollectionsSort {
 
     public static void mySort(Collection<Integer> data) {
+        ArrayList<Integer> arrayList = new ArrayList<>(data);
+
+        for (int i = 0; i < arrayList.size(); i++)
+        {
+            for (int j = i + 1; j < arrayList.size(); j++)
+            {
+                if (arrayList.get(i) > arrayList.get(j))
+                {
+                    int var = arrayList.get(j);
+                    arrayList.add(j, arrayList.get(i));
+                    arrayList.add(i, var);
+                }
+            }
+        }
+        data.removeAll(data);
+        data.addAll(arrayList);
+    }
+
+    public static void minSort(Collection<Integer> data) {
         ArrayDeque<Integer> result = new ArrayDeque<>();
-
         Iterator iterator = data.iterator();
-
         while (iterator.hasNext()) {
             int min = Collections.min(data);
             data.remove(min);
@@ -17,10 +34,6 @@ public class CollectionsSort {
 
         data.removeAll(data);
         data.addAll(result);
-    }
-
-    public static void minSort(Collection<Integer> data) {
-
     }
 
     static void collSort(Collection<Integer> data) {

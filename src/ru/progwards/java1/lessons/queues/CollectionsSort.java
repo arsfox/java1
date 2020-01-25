7a5997c0a33324446455fa85f08f432dd95939df
Arseniy Lesechko <arseniy.le@yandex.ru>
2020-01-25 13:15:42 +0300

    1

--- src/ru/progwards/java1/lessons/queues/CollectionsSort.java
+++ src/ru/progwards/java1/lessons/queues/CollectionsSort.java
@@ -1,11 +1,18 @@
 package ru.progwards.java1.lessons.queues;
 
-import java.util.Collection;
+import java.util.*;
 
 public class CollectionsSort {
 
     public static void mySort(Collection<Integer> data) {
+        ArrayDeque<Integer> list = new ArrayDeque<>(data);
 
+        for(int i = 0; i < data.size(); i++){
+            int min = Collections.min(list);
+            list.remove(min);
+            list.offer(min);
+        }
+        data = list;
     }
 
     public static void minSort(Collection<Integer> data) {

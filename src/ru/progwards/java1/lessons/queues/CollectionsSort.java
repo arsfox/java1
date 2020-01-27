04869195427b7c243c7ff96c8e738603c7a38a0e
Arseniy Lesechko <a.lesechko@ibolit.pro>
2020-01-27 13:46:11 +0300

    add

--- src/ru/progwards/java1/lessons/queues/CollectionsSort.java
+++ src/ru/progwards/java1/lessons/queues/CollectionsSort.java
@@ -3,14 +3,14 @@ package ru.progwards.java1.lessons.queues;
 import java.util.*;
 
 public class CollectionsSort {
-//[4, 14, 41, 1, 6, 49, 39, 35, 42, 28, 13, 5, 0, 39, 20, 40, 16, 17, 9, 13, 37, 44, 38, 18, 40]
+
     public static void mySort(Collection<Integer> data) {
         ArrayList<Integer> arrayList = new ArrayList<>(data);
 
         for (int i = arrayList.size() - 1; i > 0; i--) {
             for (int j = 0; j < i; j++) {
                 if (arrayList.get(j) > arrayList.get(j + 1)) {
-                    Collections.swap((List<Integer>) arrayList, j, j + 1);
+                    Collections.swap(arrayList, j, j + 1);
                 }
             }
         }
@@ -37,7 +37,63 @@ public class CollectionsSort {
     }
 
     public static Collection<String> compareSort() {
-        Collection<String> result = null;
+
+        class SortMethod implements Comparable<SortMethod> {
+            String name;
+            Long time;
+            SortMethod(String name, Long time){
+                this.name = name;
+                this.time = time;
+            }
+
+            @Override
+            public int compareTo(SortMethod sortMethod) {
+                if (this.time < sortMethod.time) {
+                    return -1;
+                }
+                else if (this.time > sortMethod.time){
+                    return 1;
+                }
+                else {
+                    return 0;
+                }
+            }
+
+            @Override
+            public String toString() {
+                return this.name+" "+this.time;
+            }
+        }
+
+        ArrayList<SortMethod> sortMethod = new ArrayList<>();
+
+        ArrayList<Integer> arrayList = new ArrayList<>();
+        for (int i = 0; i < 10_000; i++) {
+            arrayList.add(new Random().nextInt(10000));
+        }
+
+        long start1 = System.currentTimeMillis();
+        ArrayList<Integer> arrayList1 = arrayList;
+        mySort(arrayList1);
+        sortMethod.add(new SortMethod("mySort", System.currentTimeMillis() - start1));
+
+        long start2 = System.currentTimeMillis();
+        ArrayList<Integer> arrayList2 = arrayList;
+        minSort(arrayList2);
+        sortMethod.add(new SortMethod("minSort", System.currentTimeMillis() - start2));
+
+        long start3 = System.currentTimeMillis();
+        ArrayList<Integer> arrayList3 = arrayList;
+        minSort(arrayList3);
+        sortMethod.add(new SortMethod("collSort", System.currentTimeMillis() - start3));
+
+        Collections.sort(sortMethod);
+
+        Collection<String> result = new ArrayList<>();
+
+        for (int i = 0; i < sortMethod.size(); i++) {
+            result.add(sortMethod.get(i).name);
+        }
         return result;
     }
 }

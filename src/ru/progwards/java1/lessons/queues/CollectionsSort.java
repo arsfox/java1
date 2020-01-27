1f31125ca6f9568fcc0833be7c1a0da9ccd0079a
Arseniy Lesechko <a.lesechko@ibolit.pro>
2020-01-27 10:40:21 +0300

    add

--- src/ru/progwards/java1/lessons/queues/CollectionsSort.java
+++ src/ru/progwards/java1/lessons/queues/CollectionsSort.java
@@ -3,21 +3,20 @@ package ru.progwards.java1.lessons.queues;
 import java.util.*;
 
 public class CollectionsSort {
-
+//[4, 14, 41, 1, 6, 49, 39, 35, 42, 28, 13, 5, 0, 39, 20, 40, 16, 17, 9, 13, 37, 44, 38, 18, 40]
     public static void mySort(Collection<Integer> data) {
         ArrayList<Integer> arrayList = new ArrayList<>(data);
 
-        for (int i = 0; i < arrayList.size(); i++)
-        {
-            for (int j = i + 1; j < arrayList.size(); j++)
-            {
-                System.out.println();
-//                if (arrayList.get(i).compareTo(arrayList.get(j)) > 0) {
-//                    Collections.swap((List<Integer>) data, i, j);
-//                }
+        for (int i = arrayList.size() - 1; i > 0; i--) {
+            for (int j = 0; j < i; j++) {
+                if (arrayList.get(j) > arrayList.get(j + 1)) {
+                    Collections.swap((List<Integer>) arrayList, j, j + 1);
+                }
             }
         }
-        System.out.println(data);
+
+        data.removeAll(arrayList);
+        data.addAll(arrayList);
     }
 
     public static void minSort(Collection<Integer> data) {

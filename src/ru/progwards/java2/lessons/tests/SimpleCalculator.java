b5bf2abd902bd2c90486e74498d4c8ad9b066744
Arseniy <ars>
2020-03-22 22:05:58 +0300

    add test

---
+++ src/ru/progwards/java2/lessons/tests/SimpleCalculator.java
@@ -0,0 +1,8 @@
+package ru.progwards.java2.lessons.tests;
+
+public class SimpleCalculator {
+    int sum(int val1, int val2)
+    int diff(int val1, int val2);
+    int mult(int val1, int val2);
+    int div(int val1, int val2);
+}

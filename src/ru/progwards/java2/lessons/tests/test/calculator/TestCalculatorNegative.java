6c421e5f6ba830281750eeb102118630a5cdacb9
Arseniy <ars>
2020-03-26 23:47:49 +0300

    add test

--- src/ru/progwards/java2/lessons/tests/test/calculator/TestCalculatorNegative.java
+++ src/ru/progwards/java2/lessons/tests/test/calculator/TestCalculatorNegative.java
@@ -5,13 +5,32 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
 import ru.progwards.java2.lessons.tests.calculator.Calculator;
 
+import java.util.Arrays;
+import java.util.List;
+
 import static org.junit.Assert.assertEquals;
 
+@RunWith(Parameterized.class)
 public class TestCalculatorNegative {
 
-    @Test(expected = Exception.class)
-    public void test() throws Exception {
-        Calculator.calculate("1+(2+3)*2+(1+1");
+    @Parameterized.Parameter()
+    public String expression;
+
+    @Parameterized.Parameters
+    public static List<Object> data() {
+        return Arrays.asList(new Object[]{
+//                "1+(2+3)*2+(1+1",
+                "1+)1",
+                "3"
+        });
     }
+
+//    @Test(expected = Exception.class)
+    public void test() throws Exception {
+        System.out.println(expression);
+        Calculator.calculate("");
+    }
+
+
 
 }

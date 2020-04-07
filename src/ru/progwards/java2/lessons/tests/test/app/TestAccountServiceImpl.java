3b958141c23fc100ed1ac84f62cff4f9aa396cb3
Arseniy <ars>
2020-04-07 17:10:50 +0300

    add test

--- src/ru/progwards/java2/lessons/tests/test/app/TestAccountServiceImpl.java
+++ src/ru/progwards/java2/lessons/tests/test/app/TestAccountServiceImpl.java
@@ -3,7 +3,6 @@ package ru.progwards.java2.lessons.tests.test.app;
 import org.junit.Before;
 import org.junit.Test;
 import ru.progwards.java2.lessons.tests.app.model.Account;
-import ru.progwards.java2.lessons.tests.app.service.StoreService;
 import ru.progwards.java2.lessons.tests.app.service.impl.AccountServiceImpl;
 import ru.progwards.java2.lessons.tests.app.service.impl.StoreServiceImpl;
 
@@ -31,10 +30,35 @@ public class TestAccountServiceImpl {
 
     @Test
     public void testDeposit() {
-        Account acc = (Account) storeServiceImpl.get().toArray()[new Random().nextInt(storeServiceImpl.get().size())];
-        accountServiceImpl.deposit(acc, Math.random()*1_000_000);
-        assertTrue(storeServiceImpl.get(acc.getId()).getAmount() );
+        Account accExpected = (Account) storeServiceImpl.get().toArray()[new Random().nextInt(storeServiceImpl.get().size())];
+        double summ = Math.random()*1_000_000;
+        double summAcc = accExpected.getAmount();
 
+        accountServiceImpl.deposit(accExpected, summ);
+
+        Double amoumtAfterDeposit = accExpected.getAmount();
+
+        assertTrue(amoumtAfterDeposit.compareTo(summ + summAcc) == 0);
+    }
+
+    @Test(expected = RuntimeException.class)
+    public void testWithdrawExeption() {
+        Account accExpected = (Account) storeServiceImpl.get().toArray()[new Random().nextInt(storeServiceImpl.get().size())];
+        accountServiceImpl.withdraw(accExpected, Double.MAX_VALUE);
+    }
+
+    @Test
+    public void testWithdraw() {
+        Account accExpected = (Account) storeServiceImpl.get().toArray()[new Random().nextInt(storeServiceImpl.get().size())];
+        double summ = Math.random()*1_000_000;
+        accExpected.setAmount(summ + summ);
+        double summAcc = accExpected.getAmount();
+
+        accountServiceImpl.withdraw(accExpected, summ);
+
+        Double amoumtAfterWithdraw = accExpected.getAmount();
+
+        assertTrue(amoumtAfterWithdraw.compareTo(summAcc - summ) == 0);
     }
 
 }

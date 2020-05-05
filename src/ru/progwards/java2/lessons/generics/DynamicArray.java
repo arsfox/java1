fd3b02d5c9729559f59c08e316c887342472b243
Arseniy <ars>
2020-05-05 21:52:44 +0300

    add homework

---
+++ src/ru/progwards/java2/lessons/generics/DynamicArray.java
@@ -0,0 +1,62 @@
+package ru.progwards.java2.lessons.generics;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
+public class DynamicArray<T>  {
+
+    private T[] array;
+    private int size;
+    private int offset;
+    private int length;
+
+    DynamicArray(int size){
+        this.size = size;
+        array = (T[]) new Object[size];
+    }
+
+    public void add(T item) {
+        updateArray();
+        if(array==null){
+            array[offset++] = item;
+            length++;
+        } else {
+            add(item);
+        }
+
+    }
+
+    public void insert(int pos, T item ){
+        updateArray();
+        if(pos > size){
+            throw new ArrayIndexOutOfBoundsException();
+        } else {
+            array[pos] = item;
+            length++;
+        }
+    }
+
+    public void remove(int pos) {
+        if(pos > size){
+            throw new ArrayIndexOutOfBoundsException();
+        } else {
+            array[pos] = null;
+            length--;
+        }
+    }
+
+    public T get(int pos) {
+        return array[pos];
+    }
+
+    public int size() {
+        return this.size;
+    }
+
+    private void updateArray() {
+        if(length==size){
+            array = Arrays.copyOf(array, array.length * 2);
+        }
+    }
+
+}
